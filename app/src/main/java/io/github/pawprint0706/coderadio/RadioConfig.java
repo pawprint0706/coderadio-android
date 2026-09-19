578b868afa8bedb9efda32593efe922c82186ec2
pawprint0706 <pawprint0706@gmail.com>
2026-09-19 11:49:20 +0900

    Bump app version to 1.0.1

--- app/src/main/java/io/github/pawprint0706/coderadio/RadioConfig.java
+++ app/src/main/java/io/github/pawprint0706/coderadio/RadioConfig.java
@@ -6,6 +6,6 @@ public final class RadioConfig {
     public static final String API = "https://coderadio-admin-v2.freecodecamp.org/api/nowplaying/coderadio";
     public static final String SITE = "https://coderadio.freecodecamp.org/";
     public static final String SOURCE = "https://github.com/pawprint0706/coderadio-on-tray";
-    public static final String USER_AGENT = "coderadio-android/1.0.0 (+unofficial; " + SITE + ")";
+    public static final String USER_AGENT = "coderadio-android/1.0.1 (+unofficial; " + SITE + ")";
     public static final long POLL_MS = 15_000;
 }
